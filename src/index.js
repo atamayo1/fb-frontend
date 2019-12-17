0bcce1f1684978a835d5146da80252066938a6a3
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-12-17 02:14:21 -0500

    i almost finish , i need to add all the update post

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {ApolloProvider} from "react-apollo-hooks";
 import {BrowserRouter as Router} from "react-router-dom";
